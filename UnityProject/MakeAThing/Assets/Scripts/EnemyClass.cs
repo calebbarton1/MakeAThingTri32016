e2a8fd4b59fdc4e245255f298ecd15271d047c52
caleb <caleb.barton@hotmail.com>
2016-09-05 15:16:41 +1000

    adding enemy

---
+++ UnityProject/MakeAThing/Assets/Scripts/EnemyClass.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyClass : MonoBehaviour {
+
+    public float maxHealth;
+    public float health;
+
+    public float damage;
+
+    public float speed;
+
+    private Transform target;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        health = maxHealth;
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        Vector2 dir = target.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(dir);    
+    }
+}
